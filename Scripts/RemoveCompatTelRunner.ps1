2019895864ead12bbb0db40bb78d4bbff9feb27b
mah_boi <61310813+MahBoiDeveloper@users.noreply.github.com>
2026-01-20 01:16:08 +0300

    Fix deletion command for CompatTelRunner.exe

--- Scripts/RemoveCompatTelRunner.ps1
+++ Scripts/RemoveCompatTelRunner.ps1
@@ -5,7 +5,7 @@ $execute = 'powershell'
 #Запись в раздел реестра, которым владеет TI (предотвращение установки Teams), и вывод whoami в текстовый файл
 $argument = '-ExecutionPolicy Bypass -Noprofile -Command "& {
     taskill /f /im CompatTelRunner.exe
-    del ‪C:\Windows\System32\CompatTelRunner.exe
+    del C:\Windows\System32\CompatTelRunner.exe
 }"'
 $action = New-ScheduledTaskAction -Execute $execute -Argument $argument
 #Создание задание в планировщике
